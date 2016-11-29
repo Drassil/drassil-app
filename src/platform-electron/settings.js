7519ba47904bf38d6f0f4e423a15fb703fe9f57f
Matteo Emili <matteo.emili14@gmail.com>
2016-11-29 23:42:01 +0100

    .

---
+++ src/platform-electron/settings.js
@@ -0,0 +1,29 @@
+var settings = require('electron-settings');
+
+settings.get('wowFolder.wowFolder').then(val => {
+		if(val != undefined)
+			$("#wowFolder").attr("placeholder", val.toString());
+})
+
+function selectWoWFolder(){
+	const {dialog} = require('electron').remote
+	wowFolder = dialog.showOpenDialog({properties: ['openDirectory']});
+	$("#wowFolder").attr("placeholder", wowFolder[0].toString());
+}
+
+function getLauncherVer()
+{
+	var app = require('electron').remote.app;
+	version = app.getVersion();
+	//console.log(version);
+	$("#info-version").html("Launcher Version: " +version);
+}
+
+
+function saveSettings(){
+	const settings = require('electron-settings');
+
+	settings.setSync('wowFolder', {
+		'wowFolder': wowFolder[0]
+	});
+}
