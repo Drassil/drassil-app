bc02ab5c81080982245876827cbf7183a1d1c57d
Matteo Emili <matteo.emili14@gmail.com>
2016-11-30 15:41:51 +0100

    integration of settings.js into settings page LOL

---
+++ src/platform-electron/settings.js
@@ -0,0 +1,44 @@
+const {BrowserWindow} = require('electron').remote;
+settingsWin = BrowserWindow.getFocusedWindow();
+    
+function getWoWFolder()
+{
+    var settings = require('electron-settings');
+    settings.get('wowFolder.wowFolder').then(val => {
+            if(val != undefined)
+                $("#wowFolder").attr("placeholder", val.toString());
+    })
+}
+    
+function appClose()
+{
+    settingsWin.close();
+}
+
+function appMinimize()
+{
+    settingsWin.minimize();
+}
+
+function selectWoWFolder(){
+	const {dialog} = require('electron').remote
+	wowFolder = dialog.showOpenDialog({properties: ['openDirectory']});
+	$("#wowFolder").attr("placeholder", wowFolder[0].toString());
+}
+
+function getLauncherVer()
+{
+	var app = require('electron').remote.app;
+	version = app.getVersion();
+	//console.log(version);
+	$("#info-version").html("Launcher Version: " +version);
+}
+
+
+function saveSettings(){
+	const settings = require('electron-settings');
+
+	settings.setSync('wowFolder', {
+		'wowFolder': wowFolder[0]
+	});
+}
