21561965ed67bec23e0a7dc057271a7ba0bb7256
Yehonal <yehonal.azeroth@gmail.com>
2016-11-30 01:21:51 +0100

    Fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-var conf_path="./src/hwcConf.js";
-var hwc_conf=require(conf_path);
-var p=require("path");
+var path_prefix = __dirname || "./";
+var conf_path = path_prefix + "/src/hwcConf.js";
+var hwc_conf = require(conf_path);
+var p = require("path");
 
-hwc_conf.paths.hwc_js_kernel_loader=hwc_conf.paths.hwc_js_kernel;
+hwc_conf.paths.hwc_js_kernel_loader = hwc_conf.paths.hwc_js_kernel;
 
 module.exports = {
-  entry: [
+    watch: true,
+    entry: [
         "modules/bower_components/webcomponentsjs/webcomponents-lite.js",
         "modules/angularjs/1_5/angular.js",
         "modules/angularjs/1_5/angular-route.js",
@@ -15,15 +17,15 @@ module.exports = {
         conf_path,
         'modules/angular-xt/plugin/index.js',
         'src/app.js'
-  ],
-  output: {
-    path: __dirname+'/output',
-    filename: 'bundle.js'
-  },
-  devtool: 'source-map',
-  resolve: {
-      root: p.resolve(__dirname),
-      alias: hwc_conf.paths,
-      extensions: ['', '.js', '.jsx']
-  }
+    ],
+    output: {
+        path: path_prefix + '/output',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        root: p.resolve(__dirname),
+        alias: hwc_conf.paths,
+        extensions: ['', '.js', '.jsx']
+    }
 };
