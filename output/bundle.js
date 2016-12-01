f864e6373a43c2981b4538e0edf879487b2aff54
Matteo Emili <matteo.emili14@gmail.com>
2016-12-01 20:30:21 +0100

    download system almost completed v5

--- output/bundle.js
+++ output/bundle.js
@@ -52,7 +52,7 @@
 	__webpack_require__(6);
 	__webpack_require__(7);
 	__webpack_require__(8);
-	__webpack_require__(40);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"src/functions.js\""); }());
 	module.exports = __webpack_require__(41);
 
 
@@ -38995,79 +38995,7 @@
 	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
 
 /***/ },
-/* 40 */
-/***/ function(module, exports) {
-
-	var Drassil = window.Drassil;
-	
-	Drassil.parseNews = function(url) {
-	
-	    $.getJSON( url, function( data ) {
-	      var title = [];
-	      var content = [];
-	      $.each( data, function( key, val ) {
-	        title.push(val['headline'].substring(12, val['headline'].length-2));
-	            content.push(val['content']);
-	      });
-	
-	            for (var i = 0; i < title.length; i++) {
-	                    document.getElementById("title-"+[i+1]).innerHTML = title[i];
-	                    document.getElementById("content-"+[i+1]).innerHTML = content[i];
-	            };
-	    });
-	
-	    $(document).ready(function() {
-	        $('.hidden2').fadeOut(1);
-	        $('.title, .content').removeClass('hidden');
-	        $('.title, .content').delay(1500).fadeIn(1000);
-	    });
-	};
-	
-	
-	(function () {
-	    function jQueryMods(){};
-	        
-	        jQueryMods.prototype.fadeInDownloadBar = function()
-	        {
-	            $(document).ready(function() {
-	                $('.download').removeClass('hidden');
-	                $('#play-btn').removeClass('hidden');
-	                $('.download').delay(1500).fadeIn(1000);
-	            });
-	        }
-	
-	        jQueryMods.prototype.fadeOutDownloadBar = function()
-	        {
-	            $(document).ready(function() {
-	                $('.download').delay(1500).fadeOut(1000);
-	                $('#play-btn').delay(1500).fadeIn(1000);
-	            });
-	        }
-	
-	        jQueryMods.prototype.fadeInPlayButton = function()
-	        {
-	            $(document).ready(function() {
-	                $('#play-btn').removeClass('hidden');
-	                $('#play-btn').fadeIn(1000);
-	            });
-	        }
-	
-	        jQueryMods.prototype.downloadPercentage = function(downloadedMB, totalMB, name, realPercent)
-	        {
-	            $("#dataDownload").html("Data downloaded: "+downloadedMB+ " MB / "+totalMB +" MB");
-	            $("#patchDownload").html("File: "+name);
-	            $(".progress-bar").html(realPercent+' %');
-	            $(".progress-bar").attr("aria-valuenow", realPercent);
-	            $(".progress-bar").attr("style", "width:"+realPercent+"%");
-	        }
-	        
-	        // export api
-	        module.exports = {
-	            jquerymods: new jQueryMods()
-	        };
-	})();
-
-/***/ },
+/* 40 */,
 /* 41 */
 /***/ function(module, exports) {
 
--- src/functions.js
+++ src/functions.js
@@ -32,7 +32,7 @@ module.exports = {
                 $('#play-btn').removeClass('hidden');
                 $('.download').delay(1500).fadeIn(1000);
             });
-        }
+        },
 
         fadeOutDownloadBar: function()
         {
@@ -40,7 +40,7 @@ module.exports = {
                 $('.download').delay(1500).fadeOut(1000);
                 $('#play-btn').delay(1500).fadeIn(1000);
             });
-        }
+        },
 
         fadeInPlayButton: function()
         {
@@ -48,7 +48,7 @@ module.exports = {
                 $('#play-btn').removeClass('hidden');
                 $('#play-btn').fadeIn(1000);
             });
-        }
+        },
 
         downloadPercentage: function(downloadedMB, totalMB, name, realPercent)
         {
