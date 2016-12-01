7726be3142f2b1395b4bcf20fdc319d44efe8779
Matteo Emili <matteo.emili14@gmail.com>
2016-12-01 20:22:59 +0100

    download system almost completed v2

--- output/bundle.js
+++ output/bundle.js
@@ -39023,39 +39023,49 @@
 	    });
 	};
 	
-	Drassil.fadeInDownloadBar = function()
-	{
-	    $(document).ready(function() {
-	        $('.download').removeClass('hidden');
-	        $('#play-btn').removeClass('hidden');
-	        $('.download').delay(1500).fadeIn(1000);
-	    });
-	}
 	
-	Drassil.fadeOutDownloadBar = function()
-	{
-	    $(document).ready(function() {
-	        $('.download').delay(1500).fadeOut(1000);
-	        $('#play-btn').delay(1500).fadeIn(1000);
-	    });
-	}
+	(function () {
+	    function jQueryMods(){};
+	        
+	        jQueryMods.prototype.fadeInDownloadBar = function()
+	        {
+	            $(document).ready(function() {
+	                $('.download').removeClass('hidden');
+	                $('#play-btn').removeClass('hidden');
+	                $('.download').delay(1500).fadeIn(1000);
+	            });
+	        }
 	
-	Drassil.fadeInPlayButton = function()
-	{
-	    $(document).ready(function() {
-	        $('#play-btn').removeClass('hidden');
-	        $('#play-btn').fadeIn(1000);
-	    });
-	}
+	        jQueryMods.prototype.fadeOutDownloadBar = function()
+	        {
+	            $(document).ready(function() {
+	                $('.download').delay(1500).fadeOut(1000);
+	                $('#play-btn').delay(1500).fadeIn(1000);
+	            });
+	        }
 	
-	Drassil.downloadPercentage = function(downloadedMB, totalMB, name, realPercent)
-	{
-	    $("#dataDownload").html("Data downloaded: "+downloadedMB+ " MB / "+totalMB +" MB");
-	    $("#patchDownload").html("File: "+name);
-	    $(".progress-bar").html(realPercent+' %');
-	    $(".progress-bar").attr("aria-valuenow", realPercent);
-	    $(".progress-bar").attr("style", "width:"+realPercent+"%");
-	}
+	        jQueryMods.prototype.fadeInPlayButton = function()
+	        {
+	            $(document).ready(function() {
+	                $('#play-btn').removeClass('hidden');
+	                $('#play-btn').fadeIn(1000);
+	            });
+	        }
+	
+	        jQueryMods.prototype.downloadPercentage = function(downloadedMB, totalMB, name, realPercent)
+	        {
+	            $("#dataDownload").html("Data downloaded: "+downloadedMB+ " MB / "+totalMB +" MB");
+	            $("#patchDownload").html("File: "+name);
+	            $(".progress-bar").html(realPercent+' %');
+	            $(".progress-bar").attr("aria-valuenow", realPercent);
+	            $(".progress-bar").attr("style", "width:"+realPercent+"%");
+	        }
+	        
+	        // export api
+	        module.exports = {
+	            jquerymods: new jQueryMods()
+	        };
+	})();
 
 /***/ },
 /* 41 */
--- src/functions.js
+++ src/functions.js
@@ -23,39 +23,46 @@ Drassil.parseNews = function(url) {
     });
 };
 
-module.exports = {
 
-    Drassil.fadeInDownloadBar = function()
-    {
-        $(document).ready(function() {
-            $('.download').removeClass('hidden');
-            $('#play-btn').removeClass('hidden');
-            $('.download').delay(1500).fadeIn(1000);
-        });
-    }
+(function () {
+    function jQueryMods(){};
+        
+        jQueryMods.prototype.fadeInDownloadBar = function()
+        {
+            $(document).ready(function() {
+                $('.download').removeClass('hidden');
+                $('#play-btn').removeClass('hidden');
+                $('.download').delay(1500).fadeIn(1000);
+            });
+        }
 
-    Drassil.fadeOutDownloadBar = function()
-    {
-        $(document).ready(function() {
-            $('.download').delay(1500).fadeOut(1000);
-            $('#play-btn').delay(1500).fadeIn(1000);
-        });
-    }
+        jQueryMods.prototype.fadeOutDownloadBar = function()
+        {
+            $(document).ready(function() {
+                $('.download').delay(1500).fadeOut(1000);
+                $('#play-btn').delay(1500).fadeIn(1000);
+            });
+        }
 
-    Drassil.fadeInPlayButton = function()
-    {
-        $(document).ready(function() {
-            $('#play-btn').removeClass('hidden');
-            $('#play-btn').fadeIn(1000);
-        });
-    }
+        jQueryMods.prototype.fadeInPlayButton = function()
+        {
+            $(document).ready(function() {
+                $('#play-btn').removeClass('hidden');
+                $('#play-btn').fadeIn(1000);
+            });
+        }
 
-    Drassil.downloadPercentage = function(downloadedMB, totalMB, name, realPercent)
-    {
-        $("#dataDownload").html("Data downloaded: "+downloadedMB+ " MB / "+totalMB +" MB");
-        $("#patchDownload").html("File: "+name);
-        $(".progress-bar").html(realPercent+' %');
-        $(".progress-bar").attr("aria-valuenow", realPercent);
-        $(".progress-bar").attr("style", "width:"+realPercent+"%");
-    }
-};
+        jQueryMods.prototype.downloadPercentage = function(downloadedMB, totalMB, name, realPercent)
+        {
+            $("#dataDownload").html("Data downloaded: "+downloadedMB+ " MB / "+totalMB +" MB");
+            $("#patchDownload").html("File: "+name);
+            $(".progress-bar").html(realPercent+' %');
+            $(".progress-bar").attr("aria-valuenow", realPercent);
+            $(".progress-bar").attr("style", "width:"+realPercent+"%");
+        }
+        
+        // export api
+        module.exports = {
+            jquerymods: new jQueryMods()
+        };
+})();
